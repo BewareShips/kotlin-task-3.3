fd85c1802efd0fd26165ec875609ddea315acbeb
Edem Nedipov <edenpolovets@gmail.com>
2021-12-06 11:27:05 +0300

    close keyword function and restyling button

--- app/src/main/java/com/example/guessnumber/MainActivity.kt
+++ app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.guessnumber
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager.LayoutParams.*
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import kotlin.random.Random.Default.nextInt
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +19,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var guessBtn: Button
     var guessNumber:Int = 0;
 
-    var random:Int = nextInt(1,1000)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +40,7 @@ class MainActivity : AppCompatActivity() {
         guessBtn.setOnClickListener {
             guessNumber = guessInputNumber.text.toString().toInt()
             guessInputNumber.text.clear()
+            closeKeyboard()
         }
 
         validateBtn.setOnClickListener {
@@ -44,12 +48,23 @@ class MainActivity : AppCompatActivity() {
             if(number < guessNumber){
                 textView.text = "You number is higher"
                 editText.text.clear()
-            }else if(number > random){
+            }else if(number > guessNumber){
                 textView.text = "You number is lower"
                 editText.text.clear()
             }else{
                 textView.text = "You was right"
             }
+            closeKeyboard()
         }
+
+    }
+
+    private fun closeKeyboard() {
+        val view = this.currentFocus;
+        if (view != null) {
+            val hideMe =  getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            hideMe.hideSoftInputFromWindow(view.windowToken,0)
+        }
+        window.setSoftInputMode(SOFT_INPUT_STATE_ALWAYS_HIDDEN)
     }
 }
