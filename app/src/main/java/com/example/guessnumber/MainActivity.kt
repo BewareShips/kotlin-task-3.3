a8a651ac51789fe935a00de55f49f2a22c803f4d
Edem Nedipov <edenpolovets@gmail.com>
2021-12-06 23:29:14 +0300

    With 2 activities

--- app/src/main/java/com/example/guessnumber/MainActivity.kt
+++ app/src/main/java/com/example/guessnumber/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.guessnumber
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager.LayoutParams.*
@@ -12,51 +13,27 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textView: TextView
-    lateinit var editText: EditText
     lateinit var guessInputNumber: EditText
-    lateinit var validateBtn: Button
     lateinit var guessBtn: Button
     var guessNumber:Int = 0;
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView = findViewById(R.id.textView)
-        editText = findViewById(R.id.editText)
         guessInputNumber = findViewById(R.id.guessInputNumber)
-        validateBtn = findViewById(R.id.submitBtn)
         guessBtn = findViewById(R.id.guessBtn)
-
-
-
-
-
-        textView.text = "Please enter you guess"
 
         guessBtn.setOnClickListener {
             guessNumber = guessInputNumber.text.toString().toInt()
-            guessInputNumber.text.clear()
-            closeKeyboard()
-        }
 
-        validateBtn.setOnClickListener {
-            val number: Int = editText.text.toString().toInt()
-            if(number < guessNumber){
-                textView.text = "You number is higher"
-                editText.text.clear()
-            }else if(number > guessNumber){
-                textView.text = "You number is lower"
-                editText.text.clear()
-            }else{
-                textView.text = "You was right"
-            }
-            closeKeyboard()
-        }
 
+            //guessInputNumber.text.clear()
+            //closeKeyboard()
+            var newScreenIntent = Intent(this, GuessActivity::class.java)
+            newScreenIntent.putExtra("number",guessNumber)
+            startActivity(newScreenIntent)
+        }
     }
 
     private fun closeKeyboard() {
